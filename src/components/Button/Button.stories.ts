76ee5312a9066070388b1551d2a19f7bdc1ff04f
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-24 22:32:43 +0300

    add: Button Big story

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import Button from './Button'
 
 const meta: Meta<typeof Button> = {
-  title: 'Example/Button',
+  title: 'UI/Button',
   component: Button,
   parameters: {
     layout: 'centered',
@@ -14,8 +14,15 @@ const meta: Meta<typeof Button> = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
     children: 'Click Me',
+  },
+}
+
+export const Big: Story = {
+  args: {
+    children: 'Click Me',
+    big: true,
   },
 }
