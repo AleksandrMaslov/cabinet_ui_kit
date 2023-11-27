9bb1ae1cd41a76d2de45a859ca6cfab14c4082b4
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-27 23:53:12 +0300

    add: Component imports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import './styles/_fonts.css'
 import './styles/_reset.css'
 import './styles/_variables.css'
 
+import Anchor from './components/Anchor/Anchor'
 import Button from './components/Button/Button'
 import Icon from './components/Icon/Icon'
+import Input from './components/Input/Input'
+import Logo from './components/Logo/Logo'
+import Marker from './components/Marker/Marker'
 
-export { Button, Icon }
+export { Anchor, Button, Icon, Input, Logo, Marker }
