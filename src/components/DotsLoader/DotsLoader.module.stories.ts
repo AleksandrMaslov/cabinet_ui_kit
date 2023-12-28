a2dbea51f2f251b4eec496f323ca6ba6c35c99d8
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-28 23:01:11 +0300

    fix: DotLoader stories

--- src/components/DotsLoader/DotsLoader.module.stories.ts
+++ src/components/DotsLoader/DotsLoader.module.stories.ts
@@ -10,10 +10,28 @@ const meta: Meta<typeof DotsLoader> = {
   },
   tags: ['autodocs'],
   argTypes: {
+    color: {
+      options: [
+        'undefined',
+        'green',
+        'orange',
+        'lightgreen',
+        'blue',
+        'milk',
+        'white',
+        'lightgrey',
+        'grey',
+      ],
+      description: 'Цветовая схема компонента',
+      table: {
+        category: 'Optional',
+        defaultValue: { summary: 'undefined' },
+      },
+    },
     className: {
       description: 'Имя внешнего CSS класса для позиционирования',
       table: {
-        category: 'Optional',
+        categolightgreyry: 'Optional',
         defaultValue: { summary: 'undefined' },
       },
     },
@@ -25,6 +43,96 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
+    color: undefined,
     className: undefined,
+  },
+}
+
+export const Green: Story = {
+  args: {
+    color: 'green',
+    className: undefined,
+  },
+}
+
+export const Orange: Story = {
+  args: {
+    color: 'orange',
+    className: undefined,
+  },
+}
+
+export const Lightgreen: Story = {
+  args: {
+    color: 'lightgreen',
+    className: undefined,
+  },
+}
+
+export const Blue: Story = {
+  args: {
+    color: 'blue',
+    className: undefined,
+  },
+}
+
+export const Milk: Story = {
+  args: {
+    color: 'milk',
+    className: undefined,
+  },
+}
+
+export const White: Story = {
+  args: {
+    color: 'white',
+    className: undefined,
+  },
+  parameters: {
+    backgrounds: {
+      default: 'green',
+      values: [
+        {
+          name: 'green',
+          value: '#565b4f',
+        },
+      ],
+    },
+  },
+}
+
+export const Lightgrey: Story = {
+  args: {
+    color: 'lightgrey',
+    className: undefined,
+  },
+  parameters: {
+    backgrounds: {
+      default: 'green',
+      values: [
+        {
+          name: 'green',
+          value: '#565b4f',
+        },
+      ],
+    },
+  },
+}
+
+export const Grey: Story = {
+  args: {
+    color: 'grey',
+    className: undefined,
+  },
+  parameters: {
+    backgrounds: {
+      default: 'green',
+      values: [
+        {
+          name: 'green',
+          value: '#565b4f',
+        },
+      ],
+    },
   },
 }
