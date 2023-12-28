98310e9e2a7e310d5e5636aed6b24868eda744f6
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-28 22:40:23 +0300

    fix: DotLoader absolute position

--- src/components/DotsLoader/DotsLoader.tsx
+++ src/components/DotsLoader/DotsLoader.tsx
@@ -3,11 +3,21 @@ import { FC } from 'react'
 import classes from './DotsLoader.module.css'
 
 interface DotsLoaderProps {
+  color?:
+    | 'green'
+    | 'orange'
+    | 'lightgreen'
+    | 'blue'
+    | 'milk'
+    | 'white'
+    | 'lightgrey'
+    | 'grey'
   className?: string
 }
 
-const DotsLoader: FC<DotsLoaderProps> = ({ className }) => {
+const DotsLoader: FC<DotsLoaderProps> = ({ color, className }) => {
   const rootClasses = [classes.dotsLoader]
+  if (color) rootClasses.push(classes[`dotsLoader_${color}`])
   if (className) rootClasses.push(className)
 
   return <span className={rootClasses.join(' ')}></span>
